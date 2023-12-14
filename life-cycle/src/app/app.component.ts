faef5a2ae8f098317f0bd2595c1218c9bd5fc5e7
Michael Mello <michaelkcms@gmail.com>
2023-12-14 05:50:13 -0300

    atualização

--- life-cycle/src/app/app.component.ts
+++ life-cycle/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TitleComponent } from "./title/title.component";
+import { PaginaInicialComponent } from "./pagina-inicial/pagina-inicial.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, TitleComponent, PaginaInicialComponent]
 })
 export class AppComponent {
   title = 'life-cycle';
